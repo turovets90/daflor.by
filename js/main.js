22d14161b792b2f41d87ebf89bfead33ef1bb582
Aleksandr <a.turovets90@gmail.com>
2020-12-18 16:14:24 +0300

    finished_homePage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
+    /* перевод картинки svg в код */
+    $('.icon img, img.icon').each(function(){
+        var $img = $(this);
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        $.get(imgURL, function(data) {
+            var $svg = $(data).find('svg');
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+            $svg = $svg.removeAttr('xmlns:a');
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+    /* end перевод картинки svg в код */
+
 
     $('.top_menu_btn').click(function () {
         $('.top_header_menu').toggleClass('open');
@@ -123,6 +142,33 @@ $(document).ready(function(){
             });
         }
 
+    });
+
+
+
+    $(".reviews_slider").slick({
+        infinite: true,
+        arrows: true,
+        dots: true,
+        //autoplay: true,
+        //speed: 3000,
+        //autoplaySpeed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
     });
 
 
